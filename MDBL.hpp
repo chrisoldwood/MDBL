88e215b4de65a5944a873bdb91b89eb3f4b11c86
Admin <admin@chrisoldwood.com>
2007-10-15 20:31:33 +0000

    Added #pragma once.

--- MDBL.hpp
+++ MDBL.hpp
@@ -11,6 +11,10 @@
 #ifndef MDBL_HPP
 #define MDBL_HPP
 
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
 /******************************************************************************
 **
 ** Standard headers.
