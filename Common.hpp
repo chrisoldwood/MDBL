41a0222ba7df9473278b60574ad93b1823aeca35
Admin <admin@chrisoldwood.com>
2007-10-15 20:30:13 +0000

    Initial version.

---
+++ Common.hpp
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   Common.hpp
+//! \brief  Wrapper to include the most common MDBL library headers.
+//! \author Chris Oldwood
+
+// Check for previous inclusion
+#ifndef MDBL_COMMON_HPP
+#define MDBL_COMMON_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// Library documentation
+//
+//! \namespace MDBL
+//! \brief     The Memory Database Library namespace.
+
+////////////////////////////////////////////////////////////////////////////////
+// Standard headers.
+
+#include <Core/Common.hpp>		// Core library common headers.
+#include <WCL/Common.hpp>		// Windows C++ library common headers.
+
+////////////////////////////////////////////////////////////////////////////////
+// Library headers.
+
+#endif // MDBL_COMMON_HPP
