ae925107ea5d2517b33c726f6b812f4ec7bc1c71
Admin <admin@chrisoldwood.com>
2007-11-06 14:52:04 +0000

    Initial version.

---
+++ FwdDecls.hpp
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   FwdDecls.hpp
+//! \brief  Forward declarations for the most common types.
+//! \author Chris Oldwood
+
+// Check for previous inclusion
+#ifndef MDBL_FWDDECLS_HPP
+#define MDBL_FWDDECLS_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// Forward declarations.
+
+class CTimeStamp;
+class CValue;
+class CValueSet;
+class CField;
+class CColumn;
+class CColumnSet;
+class CRow;
+class CRowSet;
+class CTable;
+class CIndex;
+class CMDB;
+class CResultSet;
+class CWhere;
+class CJoin;
+class CJoinedSet;
+class CGroupSet;
+class CSQLSource;
+class CSQLParams;
+class CSQLCursor;
+
+#endif // MDBL_FWDDECLS_HPP
